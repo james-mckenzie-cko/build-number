a7011a7f3ad19f75432a62eafa63bcd965330dad
James McKenzie <james.mckenzie@checkout.com>
2020-01-15 12:06:25 +0000

    get Release Tags

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,45 @@
 import * as core from '@actions/core';
+import axios from 'axios';
 
-const doSomething = () => {
-  core.getInput('token');
-  core.setOutput('build_number', '1');
+async function main() {
+  const token = core.getInput('token');
+  const tagPrefix = core.getInput('prefix');
 
-  console.log('hello world');
-};
+  const instance = axios.create({
+    baseURL: 'https://api.github.com',
+  });
 
-doSomething();
+  instance.defaults.headers.common['Authorization'] = `token ${token}`;
+
+  const releaseTags = await getReleaseTags();
+
+  console.log(releaseTags);
+
+  async function getReleaseTags() {
+    const result = await instance.get<
+      {
+        name: string;
+      }[]
+    >(`/repos/${process.env.GITHUB_REPOSITORY}/tags`);
+
+    if (result.data) {
+      const regExpString = `/${tagPrefix}(\\d+)$`;
+      const regExp = new RegExp(regExpString);
+
+      return result.data.filter(x => regExp.test(x.name));
+    }
+
+    return [];
+  }
+}
+
+/*
+    get all tags that match pattern
+    if no tags, set to 1, end.
+
+    order tags, get latest
+    create new tag of latest + 1
+    push new tag
+*/
+
+main();
